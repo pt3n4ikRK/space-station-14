0ed80cab1147b4755e680e3ba9d8a6041d10f86c
pt3n4ikRK <97797470+pt3n4ikRK@users.noreply.github.com>
2024-05-26 02:29:41 +0200

    Update SayCommand.cs

--- Content.Server/Chat/Commands/SayCommand.cs
+++ Content.Server/Chat/Commands/SayCommand.cs
@@ -1,7 +1,10 @@
 using Content.Server.Chat.Systems;
 using Content.Shared.Administration;
+using RestSharp;
 using Robust.Shared.Console;
 using Robust.Shared.Enums;
+using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Content.Server.Chat.Commands
 {
@@ -12,30 +15,37 @@ namespace Content.Server.Chat.Commands
         public string Description => "Send chat messages to the local channel or a specified radio channel.";
         public string Help => "say <text>";
 
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        public async void Execute(IConsoleShell shell, string argStr, string[] args)
         {
+            // Перевірка чи команду викликав гравець
             if (shell.Player is not { } player)
             {
                 shell.WriteError("This command cannot be run from the server.");
                 return;
             }
 
+            // Перевірка чи гравець знаходиться в грі
             if (player.Status != SessionStatus.InGame)
                 return;
 
-            if (player.AttachedEntity is not {} playerEntity)
+            // Перевірка чи гравець має прив'язану сутність
+            if (player.AttachedEntity is not { } playerEntity)
             {
                 shell.WriteError("You don't have an entity!");
                 return;
             }
 
+            // Перевірка чи є аргументи
             if (args.Length < 1)
                 return;
 
+            // З'єднання аргументів у повідомлення
             var message = string.Join(" ", args).Trim();
             if (string.IsNullOrEmpty(message))
                 return;
 
+
+            // Відправка перекладеного повідомлення в чат
             IoCManager.Resolve<IEntitySystemManager>().GetEntitySystem<ChatSystem>()
                 .TrySendInGameICMessage(playerEntity, message, InGameICChatType.Speak, ChatTransmitRange.Normal, false, shell, player);
         }
